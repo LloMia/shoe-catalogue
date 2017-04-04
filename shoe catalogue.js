f36fb8d19bd2f2f8457238722c66610b6e7d4374
Loyiso <Loyiso@projectcodex.co>
2017-04-04 11:40:40 +0200

    save changes

--- shoe catalogue.js
+++ shoe catalogue.js
@@ -1,23 +1,57 @@
-var input = document.querySelector('.list')
-var output = document.querySelector('#output').innerHTML;// getting an Id to display output
+var input = document.querySelectorAll('#option');
+var output = document.querySelector('#display');// getting an Id to display output
+
+var stock = [
+                        {brand : "Nike", color : "black & white", inStock :65, sizes : 4},
+                        {brand : "Vans", color : "black & white", inStock :65, sizes : 4},
+                        {brand : "Puma", color : "black & white", inStock :65, sizes : 4},
+                        {brand : "Converse", color : "blue & white", inStock : 45, sizes : 9},
+                        {brand : "Adidas", color : "black", inStock : 30, sizes :  7},
+                        {brand : "Nike", color: "white", inStock : 89, sizes : 8}
+
+];
+
+var source = document.getElementById('output').innerHTML;
+var template = Handlebars.compile(source);// compiling my output to
+
+function search(){
+var availStock = [];
+  var option = document.getElementById('option').value;
+  var nike = document.getElementById('Nike').innerHTML;
+  var Puma = document.getElementById('Puma').innerHTML;
+  var Converse = document.getElementById('Converse').innerHTML;
+  var Adidas = document.getElementById('Adidas').innerHTML;
+  var Vans = document.getElementById('Vans').innerHTML;
 
 
-var availableStock = []
-
-var template = Handlebars.compile(output);// compiling my output to
-var stock = template ({
-
-  dataStock: [
-                {brand : "Nike", color : "black & white", inStock :65, sizes : 4},
-                {brand : "nike", color : "black & white", inStock :65, sizes : 4},
-                {brand : "nike", color : "black & white", inStock :65, sizes : 4},
-                {brand : "nike", color : "blue & white", inStock : 45, sizes : 9},
-                {brand : "nike", color : "black", inStock : 30, sizes :  7},
-                {brand : "nike", color: "white", inStock : 89, sizes : 8}
-
-]
+  for (var  i = 0; i < stock.length; i++){
+//  availStock.push(stock[i]);
+if (stock[i].brand === option.value){
+  availStock.push(stock[i])
 }
-);
+}
+
+output.innerHTML = template({stock});
+//console.log(availStock);
+}
+/*
 
 
-document.body.innerHTML += stock;
+ function search(){;
+  //  var option.value = option.value;
+  var availStock = [];
+
+  for (var  i = 0; i < stock.length; i++){
+    availStock.push(stock[i]);
+
+    var newStock = template ({
+      availStock
+    })
+    // if (stock[i].brand === option.value){
+      // return stock.brand == option.value
+    // }
+
+    output.innerHTML += newStock;
+
+  }
+};*/
