0cbb78d2c86617c73d06ce99995e5fb113843e39
Loyiso <Loyiso@projectcodex.co>
2017-04-05 15:58:34 +0200

    save changes

--- shoe catalogue.js
+++ shoe catalogue.js
@@ -1,14 +1,16 @@
 var input = document.querySelectorAll('#option');
 var output = document.querySelector('#display');
-var option = document.getElementById('option');
+var option1 = document.getElementById('option1');
+var option2 = document.getElementById('option2');
+var option3 = document.getElementById('option3');
 
 var stock = [
-  {brand : "Nike", color : "black & white", inStock :65, sizes : 4},
-  {brand : "Vans", color : "black & white", inStock :65, sizes : 4},
-  {brand : "Puma", color : "black & white", inStock :65, sizes : 4},
-  {brand : "Converse", color : "blue & white", inStock : 45, sizes : 9},
-  {brand : "Adidas", color : "black", inStock : 30, sizes :  7},
-  {brand : "Nike", color: "white", inStock : 89, sizes : 8}
+  {brand : "Nike", color : "black and white", inStock :65, sizes : 4, Price : 1299},
+  {brand : "Vans", color : "black and white", inStock :65, sizes : 4, Price : 749},
+  {brand : "Puma", color : "black and white", inStock :65, sizes : 4, Price : 2599},
+  {brand : "Converse", color : "blue and white", inStock : 45, sizes : 9, Price : 999 },
+  {brand : "Adidas", color : "black", inStock : 30, sizes :  7, Price : 1499},
+  {brand : "Nike", color: "white", inStock : 89, sizes : 8, Price : 1799}
 
 ];
 
@@ -22,13 +24,14 @@ var template = Handlebars.compile(source);
   var availStock = [];
 
   for (var  i = 0; i < stock.length; i++){
-    if (stock[i].brand === option.value){
+    if (stock[i].brand === option1.value && stock[i].color === option2.value && stock[i].sizes === Number(option3.value)){
 
       availStock.push(stock[i]);
     }
 
 
   }
+
   var newStock = template ({
     availStock
   })
