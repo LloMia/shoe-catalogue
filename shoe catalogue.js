e4fa53597d09a5e739bd69998df4a5f02c07a4af
Loyiso <Loyiso@projectcodex.co>
2017-04-05 11:40:32 +0200

    save changes

--- shoe catalogue.js
+++ shoe catalogue.js
@@ -1,57 +1,36 @@
 var input = document.querySelectorAll('#option');
-var output = document.querySelector('#display');// getting an Id to display output
+var output = document.querySelector('#display');
+var option = document.getElementById('option');
 
 var stock = [
-                        {brand : "Nike", color : "black & white", inStock :65, sizes : 4},
-                        {brand : "Vans", color : "black & white", inStock :65, sizes : 4},
-                        {brand : "Puma", color : "black & white", inStock :65, sizes : 4},
-                        {brand : "Converse", color : "blue & white", inStock : 45, sizes : 9},
-                        {brand : "Adidas", color : "black", inStock : 30, sizes :  7},
-                        {brand : "Nike", color: "white", inStock : 89, sizes : 8}
+  {brand : "Nike", color : "black & white", inStock :65, sizes : 4},
+  {brand : "Vans", color : "black & white", inStock :65, sizes : 4},
+  {brand : "Puma", color : "black & white", inStock :65, sizes : 4},
+  {brand : "Converse", color : "blue & white", inStock : 45, sizes : 9},
+  {brand : "Adidas", color : "black", inStock : 30, sizes :  7},
+  {brand : "Nike", color: "white", inStock : 89, sizes : 8}
 
 ];
 
-var source = document.getElementById('output').innerHTML;
-var template = Handlebars.compile(source);// compiling my output to
-
-function search(){
 var availStock = [];
-  var option = document.getElementById('option').value;
-  var nike = document.getElementById('Nike').innerHTML;
-  var Puma = document.getElementById('Puma').innerHTML;
-  var Converse = document.getElementById('Converse').innerHTML;
-  var Adidas = document.getElementById('Adidas').innerHTML;
-  var Vans = document.getElementById('Vans').innerHTML;
+var source = document.getElementById('output').innerHTML;
+var template = Handlebars.compile(source);
 
 
-  for (var  i = 0; i < stock.length; i++){
-//  availStock.push(stock[i]);
-if (stock[i].brand === option.value){
-  availStock.push(stock[i])
-}
-}
+ function search(){
 
-output.innerHTML = template({stock});
-//console.log(availStock);
-}
-/*
-
-
- function search(){;
-  //  var option.value = option.value;
   var availStock = [];
 
   for (var  i = 0; i < stock.length; i++){
-    availStock.push(stock[i]);
+    if (stock[i].brand === option.value){
 
-    var newStock = template ({
-      availStock
-    })
-    // if (stock[i].brand === option.value){
-      // return stock.brand == option.value
-    // }
+      availStock.push(stock[i]);
+    }
 
-    output.innerHTML += newStock;
 
   }
-};*/
+  var newStock = template ({
+    availStock
+  })
+    output.innerHTML = newStock;
+};
