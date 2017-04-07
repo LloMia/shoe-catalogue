e563281939754e43bcd80b51a0215667be139aa6
Loyiso <Loyiso@projectcodex.co>
2017-04-07 14:30:31 +0200

    save changes

--- shoe catalogue.js
+++ shoe catalogue.js
@@ -3,6 +3,21 @@ var output = document.querySelector('#display');
 var option1 = document.getElementById('option1');
 var option2 = document.getElementById('option2');
 var option3 = document.getElementById('option3');
+var counter = 0;
+var user = document.getElementById('user')
+var owner = document.getElementById('owner')
+
+function showHideStock(){
+  counter ++;
+
+  if (counter % 2 == 0){
+  owner.style.display='none'
+}
+else{
+  owner.style.display='block'
+}
+}
+
 
 var stock = [
   {brand : "Nike", color : "black and white", inStock :65, sizes : 4, Price : 1299},
@@ -21,7 +36,6 @@ var template = Handlebars.compile(source);
 
  function search(){
 
-  var availStock = [];
 
   for (var  i = 0; i < stock.length; i++){
     if (stock[i].brand === option1.value && stock[i].color === option2.value && stock[i].sizes === Number(option3.value)){
@@ -37,3 +51,40 @@ var template = Handlebars.compile(source);
   })
     output.innerHTML = newStock;
 };
+
+
+/*function login (){
+  var attempt = 3
+  var username = document.querySelector('#uname').value
+  var password = document.querySelector('#psw').value
+if (username == "Mi Casa" && password == "12345") {
+  window.location = "stock.html";
+  alert("successfully Logged in");
+  return false;
+}
+else{
+  attempt --;
+alert("You have left "+attempt+" attempt;");
+
+if( attempt == 0){
+document.getElementById("uname").disabled = true;
+document.getElementById("psw").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+};*/
+
+function addStock(){
+  var brandInput = document.getElementById('brand').value
+  var colorInput = document.getElementById('color').value
+  var sizesInput = document.getElementById('size').value
+  var priceInput = document.getElementById('Price').value
+
+  var freshStock = JSON.stringify({
+    brand : brandInput, color : colorInput, Price : priceInput, sizes : sizesInput
+  })
+  availStock = freshStock
+  stock.push(availStock);
+output.innerHTML = freshStock
+}
